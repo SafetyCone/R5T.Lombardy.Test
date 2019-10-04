52948ce4a5fb644b462f8e7041113d47845595fa
MinexAutomation <minex.automation@gmail.com>
2019-10-04 10:11:34 -0700

    Added stringly-typed path GetFileName() test-fixture.

---
+++ source/R5T.Lombardy.Test/Code/Test Fixtures/StringlyTypedPathGetFileNameTestFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using R5T.Salamis;
+
+
+namespace R5T.Lombardy.Test
+{
+    public class StringlyTypedPathGetFileNameTestFixture
+    {
+        #region Test Fixture
+
+        public IStringlyTypedPathOperator StringlyTypedPathOperator { get; set; }
+
+
+        public StringlyTypedPathGetFileNameTestFixture(IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// Does getting the file name from a basic Windows file path work?
+        /// </summary>
+        [TestMethod]
+        public void BasicWindows()
+        {
+            var filePath = ExampleFilePaths.WindowsFilePath1;
+            var expectedFileName = ExampleFileNames.FileName1;
+
+            var actualFileName = this.StringlyTypedPathOperator.GetFileName(filePath);
+
+            Assert.AreEqual(expectedFileName, actualFileName);
+        }
+    }
+}
